375f4da228999c7f993c6912a0d74f86b6ebf69e
이시현 <83636532+shihyun-Lee@users.noreply.github.com>
2023-03-01 22:37:54 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,16 @@
+#2444 별찍기
+
+n=int(input())
+
+for i in range(n):
+  for j in range(n-i-1):
+    print(" ",end="")
+  for k in range(2*i+1):
+    print("*",end="")
+  print("")
+for i in range(n-1,0,-1):
+  for j in range(n-i):
+    print(" ",end="")
+  for k in range(2*i-1):
+    print("*",end="")
+  print("")
